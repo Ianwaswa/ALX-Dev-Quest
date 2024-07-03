0f7bf6e0d3e4beb90df906f698d320bb0df8d059
Ianwaswa <ianwaswa2019@gmail.com>
2024-07-03 10:32:25 +0300

    Refactor code to convert decimal number to octal

--- 4-print_hexa.py
+++ 4-print_hexa.py
@@ -1,3 +1,14 @@
-for  number in range (99):
-    print(f'{number:02d} {number:x}')
-print(f'{number:98d} {number:98x}')
+decimal_number = int(input("Enter any whole number: "))
+def decimal_to_octal(num):
+    
+    octal_value = ""
+    
+    while num > 0:
+        remainder = num % 8
+        octal_value = octal_value + str(remainder)
+        num = num // 8
+    return octal_value[::-1]
+
+output = decimal_to_octal(decimal_number)
+
+print("The octal equivalent of", decimal_number, "is:", output)
