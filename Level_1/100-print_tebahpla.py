10b97939aedc764efbfaacee78f2e20338837c80
Ianwaswa <ianwaswa2019@gmail.com>
2024-07-06 11:42:09 +0300

    Refactor code to use hexadecimal format for printing numbers

--- Level_1/100-print_tebahpla.py
+++ Level_1/100-print_tebahpla.py
@@ -1,5 +1,3 @@
 for i in range(122, 97, -1):
     if (122 - i) % 2 == 0:
-        print(chr(i - 32), end='')
-    else:
-        print(chr(i), end='')
+        print("{}".format(chr(i - 32), end=''))
