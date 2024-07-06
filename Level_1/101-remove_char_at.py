591a205b9bc0a72c9d93c7e15130de2059eaf55e
Ianwaswa <ianwaswa2019@gmail.com>
2024-07-06 12:24:51 +0300

    Refactor code to remove character at specified index and update code

--- Level_1/101-remove_char_at.py
+++ Level_1/101-remove_char_at.py
@@ -2,4 +2,4 @@ def remove_char_at(str, n):
     if n < 0 and n >= len(str):
         return str
     return str[:n] + str[n+1:]
-print(remove_char_at("C is fun!", 0)) # is fun!
+print(remove_char_at("OpenAI", 5)) # OpenA
