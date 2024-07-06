45aa74e180bff5c29b5a83bb9d52fe103e80b5c7
Ianwaswa <ianwaswa2019@gmail.com>
2024-07-06 12:46:40 +0300

    Refactor code to replace vowels with asterisks

--- Level_1/replace_vowels.py
+++ Level_1/replace_vowels.py
@@ -1,10 +1,10 @@
 def replace_vowels(str):
     vowels = "AEIOUaeiou"
     result = ""
-    for char in str:
-        if char in str:
+    for vowel in vowels:
+        if vowel in vowels:
             result += "*"
         else:
-            result += char
-    return result
+            result += vowel
+    return str
 print(replace_vowels("OpenAI")) # *p*n**
