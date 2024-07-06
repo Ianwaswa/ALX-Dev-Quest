0e6090deb2a1189748eb8ba820caa0548f05e09e
Ianwaswa <ianwaswa2019@gmail.com>
2024-07-06 11:48:17 +0300

    Refactor code to use consistent formatting for printing characters

--- Level_1/100-print_tebahpla.py
+++ Level_1/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 for i in range(122, 97, -1):
     if (122 - i) % 2 != 0:
-        print(chr(i - 32), end=" ")
+        print(chr(i - 32), end="")
     else:
-        print(chr(i), end=" ")
+        print(chr(i), end="")
