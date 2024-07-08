caec5e62aee153ad8da9d78a42776d0821f41d64
Ianwaswa <ianwaswa2019@gmail.com>
2024-07-08 13:02:56 +0300

    Refactor calculation script to use calculator_1 module

--- Level_2/1-calculation.py
+++ Level_2/1-calculation.py
@@ -1,14 +1,10 @@
 if __name__ == "__main__":
-    import calculator_1
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     
-    sum = calculator_1.add(a, b)
-    difference = calculator_1.sub(a, b)
-    product = calculator_1.mul(a, b)
-    quotient = calculator_1.div(a, b)
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
     
-print("{} + {} = {}".format(a, b, sum))
-print("{} - {} = {}".format(a, b, difference))
-print("{} * {} = {}".format(a, b, product))
-print("{} / {} = {}".format(a, b, quotient))
