a58cda5251744f3f50afc7b940ab4bc465bb4cb9
Ianwaswa <ianwaswa2019@gmail.com>
2024-07-09 14:33:36 +0300

    Refactor code to improve argument handling in 2-args.py

--- Level_2/2-args.py
+++ Level_2/2-args.py
@@ -2,16 +2,16 @@ if __name__ == "__main__":
     
     import sys
     argv = sys.argv[1:]
-    arguments = sys.argv
-    arguments_number = len(arguments)
+    arguments_number = len(argv)
     
-    if len(arguments) == 0:
+    if arguments_number == 0:
         print(f"{arguments_number} arguments.")
-    elif len(arguments) == 1:
+    elif arguments_number == 1:
         print(f"{arguments_number} argument:")
-        print(f"{arguments[0]}")
-    elif len(arguments) > 1:
+        print(f"1: {argv[0]}")
+    else:
         print(f"{arguments_number} arguments:")
-        for i in range(1, arguments_number):
-            print(f"{arguments[i]}")
+        for i in range(arguments_number):
+            print(f"{i + 1}: {argv[i]}")
+
         
