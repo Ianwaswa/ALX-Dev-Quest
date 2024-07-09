d440c7ba7be6a85816d0e381f0273596e0a81435
Ianwaswa <ianwaswa2019@gmail.com>
2024-07-09 19:03:59 +0300

    Refactor code to import and print names from hidden_4 module

--- Level_2/4-hidden_discovery.py
+++ Level_2/4-hidden_discovery.py
@@ -1,3 +1,18 @@
 if __name__ == "__main__":
+    import importlib.util
     import sys
+    
+    hidden_4_path = "Level_2.hidden_4"
+    
+    spec = importlib.util.spec_from_file_location(hidden_4_path, hidden_4_path)
+    hidden_4 = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(hidden_4)
+    
+    names = dir(hidden_4)
+    
+    filtered_names = [name for name in names if not name.startswith("__")]
+    
+    for name in filtered_names:
+        print(name)
+    
     
