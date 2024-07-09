f48b0422ad7611e5113f845cea279bce123cd2c3
Ianwaswa <ianwaswa2019@gmail.com>
2024-07-09 13:54:15 +0300

    Refactor code to handle command line arguments

---
+++ Level_2/2-args.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv
+    arguments_number = len(arguments)
+    if len(arguments) == 0:
+        print(f"{arguments_number} argument")
+        
