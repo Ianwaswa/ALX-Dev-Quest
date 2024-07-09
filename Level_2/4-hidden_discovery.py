91213856f498c6bc738e397afc3d4e73c60f3adb
Ianwaswa <ianwaswa2019@gmail.com>
2024-07-09 19:05:52 +0300

    Refactor code to update hidden_4 module path

--- Level_2/4-hidden_discovery.py
+++ Level_2/4-hidden_discovery.py
@@ -2,7 +2,7 @@ if __name__ == "__main__":
     import importlib.util
     import sys
     
-    hidden_4_path = "Level_2.hidden_4"
+    hidden_4_path = "hidden_4.pyc"
     
     spec = importlib.util.spec_from_file_location(hidden_4_path, hidden_4_path)
     hidden_4 = importlib.util.module_from_spec(spec)
