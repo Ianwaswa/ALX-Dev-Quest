882462ac2e439de1ff6da21805ebf6b719c9f81e
Ianwaswa <ianwaswa2019@gmail.com>
2024-07-09 14:06:27 +0300

    Refactor code to handle command line arguments and improve argument handling in 2-args.py

--- Level_2/2-args.py
+++ Level_2/2-args.py
@@ -2,11 +2,14 @@ if __name__ == "__main__":
     import sys
     arguments = sys.argv
     arguments_number = len(arguments)
+    
     if len(arguments) == 0:
         print(f"{arguments_number} arguments.")
     elif len(arguments) == 1:
         print(f"{arguments_number} argument:")
         print(f"{arguments[0]}")
-    else:
-        print(f"{arguments_number} arguments:/n{arguments}")
+    elif len(arguments) > 1:
+        print(f"{arguments_number} arguments:")
+        for i in range(1, arguments_number):
+            print(f"{arguments[i]}")
         
