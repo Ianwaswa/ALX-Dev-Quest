ce28a858e9dd194276bf9e4ac0062264167083a6
Ianwaswa <ianwaswa2019@gmail.com>
2024-08-23 10:32:41 +0300

    Refactor code to improve command line argument handling in fibo.py

--- Level_2/fibo.py
+++ Level_2/fibo.py
@@ -2,8 +2,8 @@ import sys
 
 n = len(sys.argv)
 print("Total arguments passed:", n)
-print("\nArguments passed:", end=" ")
 print("\nName of the script:", sys.argv[0])
+print("\nArguments passed:", end=" ")
 
 for i in range(1, n):
     print(sys.argv[i], end=" ")
