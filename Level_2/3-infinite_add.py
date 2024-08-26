6352e9ac467ee30d2de5c07d4affbf51bbcc3e8d
Ianwaswa <ianwaswa2019@gmail.com>
2024-08-26 05:40:29 +0300

    Refactor code to improve command line argument handling in 3-infinite_add.py

--- Level_2/3-infinite_add.py
+++ Level_2/3-infinite_add.py
@@ -2,5 +2,10 @@ if __name__ == "__main__":
     
     import sys
     argv = sys.argv[1:]
-    sum_total = sum(int(arg) for arg in argv)
-    print(f"{sum_total}")
+    arg_count = len(argv)
+    result = 0
+    
+    for i in range(1, arg_count):
+        result += int(arg_count)
+    
+    print(f"Result: {result}")
