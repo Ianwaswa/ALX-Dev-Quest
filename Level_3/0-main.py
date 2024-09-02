9faffbb8657da8995c21b713ec3daae9f9980469
Ianwaswa <ianwaswa2019@gmail.com>
2024-09-02 10:14:57 +0300

    Refactor code to check for presence of number 6 in the concatenated list

--- Level_3/0-main.py
+++ Level_3/0-main.py
@@ -1,6 +1,6 @@
 squares = [1, 2, 3, 4]
 concatenation_operation = squares + [5, 6, 7, 8]
-if 9 in concatenation_operation:
+if 6 in concatenation_operation:
     print("Yes, 9 is in the list")
 else:
     print("No, 9 is not in the list")
