36f6fcd260bb478f0d4e1ab4f4abe3d5a8d5617c
Ianwaswa <ianwaswa2019@gmail.com>
2024-09-03 12:33:08 +0300

    Refactor code to add print_reversed_list_integer function in Level_3/3-print_reversed_list_integer.py

---
+++ Level_3/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+def print_reversed_list_integer(my_list=[]):
+    my_list = [1, 2, 3, 4, 5]
+    reversed_list = my_list.reverse()
+    return reversed_list
+    print(reversed_list)
+print_reversed_list_integer(my_list = '')
