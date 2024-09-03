bf7f6fb3c03c53611f5b4aee939395d5f1f1143d
Ianwaswa <ianwaswa2019@gmail.com>
2024-09-03 12:01:06 +0300

    Refactor replace_in_list function to handle out-of-range index

--- Level_3/2-replace_in_list.py
+++ Level_3/2-replace_in_list.py
@@ -1,12 +1,14 @@
 def replace_in_list(my_list, idx, element):
-    my_list = [1, 2, 3, 4, 5]
-    idx = int(input("Enter an index to replace the element: "))
-    element = input("Enter the new element: ")
-    my_list[idx] = element
-    if idx < 0:
-        return my_list
-    elif idx >= len(my_list):
+    if idx < 0 or idx >= len(my_list):
+        print("Index is out of range.")
         return my_list
     else:
-        print("The new list is", my_list)
-replace_in_list(my_list = '', idx = '', element = '')
+        my_list[idx] = element
+        return my_list
+    
+my_list = [1, 2, 3, 4, 5]
+idx = int(input("Enter an index to replace the element: "))
+element = input("Enter the new element: ")
+
+new_list = replace_in_list(my_list, idx, element)
+print("The new list is:", new_list)
