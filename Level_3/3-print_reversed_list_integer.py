ad75afdb94cc3475772d7130b5d0c7b781cb294a
Ianwaswa <ianwaswa2019@gmail.com>
2024-09-10 07:41:11 +0300

    Refactor code to add print statement for the original list in print_reversed_list_integer function

--- Level_3/3-print_reversed_list_integer.py
+++ Level_3/3-print_reversed_list_integer.py
@@ -2,5 +2,6 @@ def print_reversed_list_integer(my_list=[]):
     my_list = [1, 2, 3, 4, 5]
     for numbers in reversed(my_list):
         print(numbers)
+    print(my_list)
 
 print_reversed_list_integer()
