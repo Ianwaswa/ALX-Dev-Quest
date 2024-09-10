ca2411932ce60552144929d4ccb32a7fed618c69
Ianwaswa <ianwaswa2019@gmail.com>
2024-09-10 07:40:02 +0300

    Refactor code to print each integer in reversed list using the reversed() function

--- Level_3/3-print_reversed_list_integer.py
+++ Level_3/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 def print_reversed_list_integer(my_list=[]):
     my_list = [1, 2, 3, 4, 5]
-    my_list.reverse()
-    for i in my_list:
-        print(i)
+    for numbers in reversed(my_list):
+        print(numbers)
 
 print_reversed_list_integer()
